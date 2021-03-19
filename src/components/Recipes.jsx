5801894443dc0bc9c162688761c40061111fe532
mustabur <mustaburrahman2017@hotmail.com>
2021-03-19 14:55:22 +0000

    added styling to recipes component

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Recipes({title, calories, image, ingredients}) {
+    
+    const [btn, setBtn] = useState(false)
+
+    const handleClick = () => {
+        setBtn(!btn);
+    }
+ 
+
+
     return (
-        <div>
+        <div className="recipes">
             <img src={image} alt={title}/>
             <h2>{title}</h2>
             <p>{Math.round(calories)}</p>
+            <button onClick={handleClick}>View Ingredients</button>
+            {btn && ingredients.map(ingredient => {
+                return (
+                    <p>{ingredient.text}</p>
+                )
+            })}
         </div>
     )
 }
