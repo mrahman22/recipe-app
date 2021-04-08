5e9d94e09bdcb43c57e7655e8ee5cfa3c1bebac8
mustabur <mustaburrahman2017@hotmail.com>
2021-04-08 15:57:19 +0100

    second unit test created and passed which ensure SearchForm handles changes on input field

--- src/test/SearchForm.test.js
+++ src/test/SearchForm.test.js
@@ -1,9 +1,22 @@
 import React from "react";
-import {render, fireEvent} from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import SearchForm from "../components/SearchForm";
 
-
 it("renders correctly", () => {
-    const {queryByTestId, queryByPlaceholderName} = render(<SearchForm />)
-    expect(queryByTestId("search-btn")).toBeTruthy();
-})
+  const { queryByTestId, queryByPlaceholderText } = render(<SearchForm />);
+  expect(queryByTestId("search-btn")).toBeTruthy();
+  expect(
+    queryByPlaceholderText("type in your main ingredient.......")
+  ).toBeTruthy();
+});
+
+describe("Input value", () => {
+  it("updates on change", () => {
+    const { queryByPlaceholderText } = render(<SearchForm />);
+    const searchInput = queryByPlaceholderText(
+      "type in your main ingredient......."
+    );
+    fireEvent.change(searchInput, { target: { value: "test" } });
+    expect(searchInput.value).toBe("test");
+  });
+});
